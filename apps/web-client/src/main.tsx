a65fd63da216b2c7dd9fc526dcd23a5ec25bc9c7
oluomoni <gbengaomoni@gmail.com>
2026-07-06 22:04:56 -0600

    Fix initSentry import: use relative path, add missing import

--- apps/web-client/src/main.tsx
+++ apps/web-client/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/queryClient";
-import { QueryErrorBoundary } from "@/lib/errorBoundary/QueryErrorBoundary";
+import { queryClient } from "./lib/queryClient";
+import { QueryErrorBoundary } from "./lib/errorBoundary/QueryErrorBoundary";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
-import { useServiceWorker } from "@/hooks/useServiceWorker";
+import { useServiceWorker } from "./hooks/useServiceWorker";
+import { initSentry } from "./lib/sentry";
 
 initSentry();
 
